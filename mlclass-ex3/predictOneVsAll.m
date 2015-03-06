8d5abf77d01de188781a38d8be5ebf28e8fde5ac
mridulgupta9 <mridulgupta9@gmail.com>
2015-03-07 02:12:57 +0530

    assignment 3

---
+++ mlclass-ex3/predictOneVsAll.m
@@ -0,0 +1,7 @@
+function p = predictOneVsAll(all_theta, X)
+m = size(X, 1);
+num_labels = size(all_theta, 1);
+p = zeros(size(X, 1), 1);
+X = [ones(m, 1), X];
+[x,p]=max((X*(all_theta')), [], 2);
+end
